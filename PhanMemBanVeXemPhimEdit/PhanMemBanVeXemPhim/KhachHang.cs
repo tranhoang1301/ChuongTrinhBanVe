25712ed37dc87f4a78d342231017cac620905bbd
karaker123 <44973382+karaker123@users.noreply.github.com>
2018-12-12 22:47:08 +0700

    Ban chinh sua

---
+++ PhanMemBanVeXemPhimEdit/PhanMemBanVeXemPhim/KhachHang.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhanMemBanVeXemPhim
+{
+    public class KhachHang
+    {
+        public string Name { get; set; }
+        public List<int> GheThuongs = new List<int>();
+        public List<string> GheVips = new List<string>();
+        public bool LaHSSV { get; set; }
+        public DateTime TGD { get; set; }
+        public double TinhTien
+        {
+            get
+            {
+                double tien = GheThuongs.Count * 50000 + GheVips.Count * 70000;
+                if (LaHSSV)
+                    tien = tien*0.8;
+                return tien;
+            }
+        }
+        public Phim PhimDat { get; set; }
+
+        public override string ToString()
+        {
+            return Name + ". Tổng số ghế đặt:" + (GheThuongs.Count+GheVips.Count);
+        }
+        public DateTime ThoiGianDat
+        {
+            get
+            {
+                DateTime TGDat = new DateTime();
+                TGDat = DateTime.Now;
+                return TGDat;
+            }
+        }
+    }
+}
---
+++ PhanMemBanVeXemPhimEdit/PhanMemBanVeXemPhim/Phim.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhanMemBanVeXemPhim
+{
+    public class Phim
+    {
+        public string TenPhim { get; set; }
+        public Phong ChieuTai { get; set; }
+        public override string ToString()
+        {
+            return TenPhim ;
+        }
+    }
+}
